9bdb65339702f284c2c7d4c6778ab4d9e230e07d
Matt Dunbar <matt@buildrx.com>
2015-04-20 10:00:39 -0400

    fixed missing options

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -16,10 +16,12 @@ module Spree
       facet_opts = {}
 
       variants.map(&:option_values).map{ |ov|
-        if facet_opts["o_#{ov.first.option_type_id}"].nil?
-          facet_opts["o_#{ov.first.option_type_id}"] = []
+        if ov.first
+          if facet_opts["o_#{ov.first.option_type_id}"].nil?
+            facet_opts["o_#{ov.first.option_type_id}"] = []
+          end
+          facet_opts["o_#{ov.first.option_type_id}"].push(ov.first.presentation)
         end
-        facet_opts["o_#{ov.first.option_type_id}"].push(ov.first.presentation)
       }
 
       facet_opts
